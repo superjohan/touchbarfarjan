927f43c0be067addbe2e31c89a674618bd6adf93
Johan Halin <johan@halin.me>
2018-09-16 14:10:28 +0300

    Initial Commit

---
+++ touchbarfarjan/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  touchbarfarjan
+//
+//  Created by Johan Halin on 16/09/2018.
+//  Copyright © 2018 Aero Deko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
